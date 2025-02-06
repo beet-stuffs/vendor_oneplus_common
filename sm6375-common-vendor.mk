84bf5ff7c31035da12cf171dac521ac53557125e
Sugakesshaa <sugakesshaa@pixelos.net>
2025-02-07 02:22:58 +0800

    sm6375-common: Sync with common tree
    Change-Id: Ifda5f828cd9bd24d3a2222b7d4791fc912e9f095

--- sm6375-common-vendor.mk
+++ sm6375-common-vendor.mk
@@ -6,11 +6,11 @@ PRODUCT_SOONG_NAMESPACES += \
     vendor/oneplus/sm6375-common
 
 PRODUCT_COPY_FILES += \
-    vendor/oneplus/sm6375-common/proprietary/odm/etc/audio_effects.xml:$(TARGET_COPY_OUT_ODM)/etc/audio_effects.xml \
     vendor/oneplus/sm6375-common/proprietary/odm/etc/dolby/multimedia_dolby_dax_default.xml:$(TARGET_COPY_OUT_ODM)/etc/dolby/multimedia_dolby_dax_default.xml \
     vendor/oneplus/sm6375-common/proprietary/odm/etc/init/android.hardware.drm@1.3-service.widevine.rc:$(TARGET_COPY_OUT_ODM)/etc/init/android.hardware.drm@1.3-service.widevine.rc \
     vendor/oneplus/sm6375-common/proprietary/odm/etc/init/vendor-oplus-hardware-performance-V1-service.rc:$(TARGET_COPY_OUT_ODM)/etc/init/vendor-oplus-hardware-performance-V1-service.rc \
-    vendor/oneplus/sm6375-common/proprietary/odm/etc/init/vendor.dolby_v3_6.hardware.dms360@2.0-service.rc:$(TARGET_COPY_OUT_ODM)/etc/init/vendor.dolby_v3_6.hardware.dms360@2.0-service.rc \
+    vendor/oneplus/sm6375-common/proprietary/odm/etc/init/vendor.dolby.media.c2@1.0-service.rc:$(TARGET_COPY_OUT_ODM)/etc/init/vendor.dolby.media.c2@1.0-service.rc \
+    vendor/oneplus/sm6375-common/proprietary/odm/etc/init/vendor.dolby_sp.hardware.dmssp@2.0-service.rc:$(TARGET_COPY_OUT_ODM)/etc/init/vendor.dolby_sp.hardware.dmssp@2.0-service.rc \
     vendor/oneplus/sm6375-common/proprietary/odm/etc/init/vendor.oplus.hardware.stability.oplus_project-V1-service.rc:$(TARGET_COPY_OUT_ODM)/etc/init/vendor.oplus.hardware.stability.oplus_project-V1-service.rc \
     vendor/oneplus/sm6375-common/proprietary/odm/etc/izat.conf:$(TARGET_COPY_OUT_ODM)/etc/izat.conf \
     vendor/oneplus/sm6375-common/proprietary/odm/etc/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_ODM)/etc/media_profiles_V1_0.xml \
@@ -840,21 +840,30 @@ PRODUCT_PACKAGES += \
     vendor.qti.imsrtpservice@3.0 \
     vendor.qti.imsrtpservice@3.1 \
     libc++_shared \
-    libdapparamstorage_v3_6 \
-    libdeccfg_v3_6 \
-    libdlbdsservice_v3_6 \
-    libqtigef_odm \
-    libhwdap_v3_6 \
-    libswdap_v3_6 \
-    vendor.dolby_v3_6.hardware.dms360@2.0-impl \
-    vendor.dolby_v3_6.hardware.dms360@2.0 \
+    libdapparamstorage_sp \
+    libdeccfg_sp \
+    libdlbdsservice_sp \
+    vendor.dolby_sp.hardware.dmssp@2.0-impl \
+    vendor.dolby_sp.hardware.dmssp@2.0 \
     vendor.oplus.hardware.performance-V1-ndk_platform \
     vendor.oplus.hardware.stability.oplus_project-V1-ndk_platform \
     vendor.oplus.hardware.stability.oplus_project@1.0 \
-    libstagefright_soft_ddpdec \
-    libstagefrightdolby \
+    libdlbpreg_sp \
+    libspatializerparamstorage \
+    libdlbvol_sp \
+    libswdap_sp \
+    libswgamedap_sp \
+    libswspatializer \
+    libcodec2_hidl@1.0_sp \
+    libcodec2_hidl_plugin_sp \
+    libcodec2_soft_ac4dec_sp \
+    libcodec2_soft_common_sp \
+    libcodec2_soft_ddpdec_sp \
+    libcodec2_store_dolby_sp \
+    libcodec2_vndk_sp \
     liboemcrypto \
     liboplus_service \
+    libui_sp \
     libwvhidl \
     libwvdrmengine \
     vendor.oplus.hardware.appradio@1.0 \
@@ -885,9 +894,9 @@ PRODUCT_PACKAGES += \
     c2_manifest_vendor.xml \
     vendor.qti.gnss@4.0-service.xml \
     manifest_android.hardware.drm@1.3-service.widevine.xml \
-    manifest_dax_dolby_v3_6.xml \
     manifest_oplus_stability_project_aidl.xml \
     manifest_performance_aidl.xml \
+    vendor.dolby.hardware.dms.xml \
     adpl \
     adsprpcd \
     cdsprpcd \
@@ -960,7 +969,8 @@ PRODUCT_PACKAGES += \
     wfdservice \
     android.hardware.drm@1.3-service.widevine \
     vendor-oplus-hardware-performance-V1-service \
-    vendor.dolby_v3_6.hardware.dms360@2.0-service \
+    vendor.dolby_sp.hardware.dmssp@2.0-service \
+    vendor.dolby_sp.media.c2@1.0-service \
     vendor.oplus.hardware.stability.oplus_project-V1-service \
     oplus_sensor_fb
 
@@ -977,3 +987,6 @@ PRODUCT_PACKAGES += \
     vendor_lib64_libEGL_adreno_so \
     vendor_lib64_libGLESv2_adreno_so \
     vendor_lib64_libq3dtools_adreno_so
+
+PRODUCT_BOOT_JARS += \
+    WfdCommon
